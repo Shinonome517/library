d4e6de62d9d51fdaf9b6da6d3067013b5cdbc586
Shinonome517 <mizopon517@gmail.com>
2021-04-03 14:49:15 +0900

    split sorcefile

--- binarySearchTree.cpp
+++ binarySearchTree.cpp
@@ -1,32 +1,4 @@
-#include <iostream>
-#include <cstdint> //多分ここにNULLが定義されているので<cstddef>はincludeしなくてもいい
-#include <new>
-#include <cmath>
-using namespace std;
-
-
-//ノード変数の実装
-struct node{
-    int64_t key;
-    node *left;
-    node *right;
-};
-
-//一つのクラスとして二分木を実装
-class Node{
-private:
-    node tree;
-    node *goDown(int64_t);
-    node *minNd(node*);
-public:
-    Node(int64_t);
-    ~Node();
-    void insert(int64_t);
-    bool search(int64_t);
-    void erase(int64_t);
-    
-};
-
+#include "binarySearchTree.h"
 /*
 二分探索木(binarySearchTree)の実装
     *挿入insert()
@@ -105,7 +77,12 @@ bool Node::search(int64_t target){
 void Node::erase(int64_t target){
     //まずはそもそも存在するのかを考える
     node *nd = goDown(target);
-    if(nd->left == NULL )
+    if(nd->left == NULL && nd->right == NULL){
+        delete nd;
+    }
+    else if(nd->left == NULL || nd->right == NULL){
+        nd
+    }
 
     }
 
---
+++ binarySearchTree.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdint> //多分ここにNULLが定義されているので<cstddef>はincludeしなくてもいい
+#include <new>
+#include <cmath>
+using namespace std;
+
+
+//ノード変数の実装
+struct node{
+    int64_t key;
+    node *left;
+    node *right;
+};
+
+//一つのクラスとして二分木を実装
+class Node{
+private:
+    node tree;
+    node *goDown(int64_t);
+    node *minNd(node*);
+public:
+    Node(int64_t);
+    ~Node();
+    void insert(int64_t);
+    bool search(int64_t);
+    void erase(int64_t);
+    
+};
