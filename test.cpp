25bcc895b3e0b4017a04b121a3915e17b7956fd1
Shinonome517 <mizopon517@gmail.com>
2021-04-03 17:16:08 +0900

    make sorcefile for test

--- test.cpp
+++ test.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
-using namespace std;
+#include "binarySearchTree.h"
+
+string TorF(bool flag){
+    if(flag){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
+    }
+}
 
 int main(){
-    cout << abc << endl；
+    Node tree(7);
+
+    tree.insert(4);
+    tree.insert(12);
+    tree.insert(3);
+    tree.insert(5);
+    tree.insert(9);
+    tree.insert(15);
+
+    TorF(tree.search(9)); //Yes
+    TorF(tree.search(7)); //Yes
+    TorF(tree.search(10)); //No
+
+    tree.erase(9);
+    TorF(tree.search(9)); //No
 }
